f9f5d19ac083b40aee73666e2a4929e8b2da1c84
chandanac2897 <chandana.c@ibadvisory.com>
2026-02-19 16:21:49 +0530

    profile edit

--- frontend/src/pages/profile/components/ProfileUpload.jsx
+++ frontend/src/pages/profile/components/ProfileUpload.jsx
@@ -1,8 +1,11 @@
 import profile_bg from '@assets/header-icons/profilebg.svg'
 import user from '@assets/dummy/center.svg'
 import { Camera } from 'lucide-react'
+import { useState } from 'react'
+import UpdateProfile from './UpdateProfile'
 
 const ProfileUpload = () => {
+  const [open, setOpen] = useState(false)
   return (
     <div className='flex flex-col'>
       {/* Background Image */}
@@ -22,7 +25,7 @@ const ProfileUpload = () => {
         />
 
         {/* Camera Button (Left Overlapping) */}
-        <button className='absolute -right-2 bottom-0 bg-primary w-8 h-8 rounded-full flex justify-center items-center shadow-md'>
+        <button onClick={() => setOpen(true)} className='absolute -right-2 bottom-0 bg-primary w-8 h-8 rounded-full flex justify-center items-center shadow-md'>
           <Camera size={16} className='text-white' />
         </button>
       </div>
@@ -32,6 +35,7 @@ const ProfileUpload = () => {
       </span>
       <span className='text-center block text-xs text-secondary'>Center Admin</span>
     </div>
+    <UpdateProfile open={open} setOpen={setOpen} />
     </div>
   )
 }
---
+++ frontend/src/pages/profile/components/UpdateProfile.jsx
@@ -0,0 +1,59 @@
+import CustomeModal from '@common/CustomeModal'
+import { Button } from '@pages/components/ui/button'
+import InputFile from '@common/CustomeFileUpload'
+import { useFormik } from 'formik'
+
+const UpdateProfile = ({ open, setOpen }) => {
+  const initialValues = {
+    image_url: null
+  }
+
+  const formik = useFormik({
+    initialValues,
+    enableReinitialize: true,
+    onSubmit: async () => {
+      try {
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  })
+
+  return (
+    <CustomeModal
+      open={open}
+      onOpenChange={setOpen}
+      header='Update Profile Photo'
+    >
+      <form onSubmit={formik.handleSubmit}>
+        <div className='flex flex-col justify-center items-center gap-4'>
+          <span className='text-grey text-sm font-normal'>
+            Prathibha, help others recognize you!
+          </span>
+          <InputFile
+            name='image_url'
+            value={formik.values.image_url}
+            onChange={e => {
+              formik.setFieldValue('image_url', e.target.value)
+              formik.setFieldTouched('image_url', true, false)
+            }}
+            onRemove={() => {
+              formik.setFieldValue('image_url', null)
+              formik.setFieldTouched('image_url', true, false)
+            }}
+          />
+          <span className='text-grey text-sm '>
+            Upload a clear photo for easy identification.
+          </span>
+        </div>
+
+        <div className='flex gap-2 justify-end pt-4'>
+          <Button size='addbutton' variant='default' type='submit'>
+            Upload Photo
+          </Button>
+        </div>
+      </form>
+    </CustomeModal>
+  )
+}
+export default UpdateProfile
