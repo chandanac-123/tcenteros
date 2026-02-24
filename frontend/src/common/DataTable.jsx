216ee07bccc9e9c2cc3b264b9a9abc41531e28a5
chandanac2897 <chandana.c@ibadvisory.com>
2026-02-24 16:49:40 +0530

    data tabel width

--- frontend/src/common/DataTable.jsx
+++ frontend/src/common/DataTable.jsx
@@ -107,7 +107,7 @@ export function DataTable ({
   return (
     <div className='overflow-hidden rounded-md border '>
       <div className='max-h-[380px] overflow-y-auto'>
-        <Table className='table-fixed w-full'>
+        <Table className=''>
           <TableHeader>
             {table?.getHeaderGroups()?.map(headerGroup => (
               <TableRow key={headerGroup?.id}>
