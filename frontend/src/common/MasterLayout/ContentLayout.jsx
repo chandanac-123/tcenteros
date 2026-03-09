65f00b0e7697fa8ba420c03f5c095c2c89f24f90
chandanac2897 <chandana.c@ibadvisory.com>
2026-03-09 15:12:54 +0530

    table scroll

--- frontend/src/common/MasterLayout/ContentLayout.jsx
+++ frontend/src/common/MasterLayout/ContentLayout.jsx
@@ -1,6 +1,6 @@
 const ContentLayout = ({ children }) => {
   return (
-    <div className='rounded-tl-2xl bg-textwhite p-4 h-full w-full'>{children}</div>
+    <div className='rounded-tl-2xl bg-textwhite p-4 h-full w-full overflow-auto'>{children}</div>
   )
 }
 export default ContentLayout
