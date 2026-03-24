2db719e1c2532d9873e57e64b2a10aab9cb43b1b
chandanac2897 <chandana.c@ibadvisory.com>
2026-03-24 15:05:36 +0530

    warning clear

--- frontend/src/common/components/CustomeModal.jsx
+++ frontend/src/common/components/CustomeModal.jsx
@@ -1,12 +1,20 @@
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger
 } from '@pages/components/ui/dialog'
 
-const CustomeModal = ({ label, header, children, open, onOpenChange, className = '' }) => {
+const CustomeModal = ({
+  label,
+  header,
+  children,
+  open,
+  onOpenChange,
+  className = ''
+}) => {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       {label && (
@@ -17,9 +25,12 @@ const CustomeModal = ({ label, header, children, open, onOpenChange, className =
         </DialogTrigger>
       )}
 
-      <DialogContent className={`w-auto max-h-[80vh] overflow-y-auto ${className}`}>
+      <DialogContent
+        className={`w-auto max-h-[80vh] overflow-y-auto ${className}`}
+      >
         <DialogHeader>
           <DialogTitle>{header}</DialogTitle>
+          <DialogDescription></DialogDescription>
         </DialogHeader>
         {children}
       </DialogContent>
