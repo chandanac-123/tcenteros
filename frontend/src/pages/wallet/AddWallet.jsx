081ef3208fc6facfe3736ae2a3a8e153eb74e7ad
chandanac2897 <chandana.c@ibadvisory.com>
2026-03-28 12:38:23 +0530

    wwallet add

--- frontend/src/pages/wallet/AddWallet.jsx
+++ frontend/src/pages/wallet/AddWallet.jsx
@@ -5,16 +5,26 @@ import { Button } from '@pages/components/ui/button'
 import { useCreateWalletAmountMutation } from '@api-queries/wallet/Query'
 import { showError, showSuccess } from '@utils/toast'
 
-const AddWallet = ({ open, setOpen, refetchWalletAmount , topUp }) => {
+const AddWallet = ({ open, setOpen, refetchWalletAmount, topUp }) => {
   const [amount, setAmount] = useState('')
   const { mutate: createWalletAmount, isPending } =
     useCreateWalletAmountMutation()
 
+
   const handleAddWalletAmount = e => {
     e.preventDefault()
-    if (!amount || Number(amount) <= 0) return
+    const numericAmount = Number(amount)
+    if (!numericAmount || numericAmount <= 0) {
+      showError('Amount must be greater than zero')
+      return
+    }
+    //  Only when topUp is FALSE
+    if (!topUp && numericAmount !== 20000) {
+      showError('Deposit amount must be exactly ₹20,000')
+      return
+    }
     createWalletAmount(
-      { deposit: Number(amount) },
+      { deposit: numericAmount },
       {
         onSuccess: () => {
           setAmount('')
@@ -37,18 +47,23 @@ const AddWallet = ({ open, setOpen, refetchWalletAmount , topUp }) => {
     <CustomeModal open={open} onOpenChange={setOpen}>
       <form className='space-y-2' onSubmit={handleAddWalletAmount}>
         <h2 className='min-w-[300px] text-[20px] leading-[30px] bg-[#F0DEFF] rounded-lg p-3'>
-           Add Wallet
+          Add Wallet
         </h2>
         <Input
           label='Amount'
           placeholder='Enter Your Amount'
           value={amount}
           onChange={e => setAmount(e.target.value)}
+         
         />
         {topUp ? (
-          <p className='text-xs text-onboard_secondary'>Wallet should have a minimum of 10,000</p>
+          <p className='text-xs text-secondary'>
+            Wallet should have a minimum of 10,000
+          </p>
         ) : (
-          <p className='text-xs text-onboard_secondary'>Deposit amount should be 20,000</p>
+          <p className='text-xs text-onboard_secondary'>
+            Deposit amount should be 20,000
+          </p>
         )}
         <div className='flex justify-center gap-3'>
           <Button
