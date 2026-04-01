b3ba97dfd90da571dc6877f17ea0fed191820cb0
chandanac2897 <chandana.c@ibadvisory.com>
2026-04-01 09:51:44 +0530

    settlement

--- frontend/src/pages/billing/component/reports/SettlementReport.jsx
+++ frontend/src/pages/billing/component/reports/SettlementReport.jsx
@@ -1,21 +1,54 @@
 import { DataTable } from '@common/components/DataTable'
 import { useSettlementReportQuery } from '@api-queries/billing/Query'
 import { Badge } from '@pages/components/ui/badge'
+import { formatDate } from '@utils/helper'
 const statusVariantMap = { completed: 'active', pending: 'inactive' }
-
+const flowVariantMap = {
+  in: 'active', // green
+  out: 'inactive' // red
+}
 const SettlementReport = ({ tableParams, setTableParams }) => {
   const { data, isLoading } = useSettlementReportQuery(tableParams)
+  console.log('data: ', data)
 
   const columns = [
-    { accessorKey: 'period_label', header: 'Period' },
-    { accessorKey: 'period_start', header: 'Start Date' },
-    { accessorKey: 'period_end', header: 'End Date' },
-    { accessorKey: 'membership_income', header: 'Membership Income' },
-    { accessorKey: 'inventory_income', header: 'Inventory Income' },
-    { accessorKey: 'networking_income', header: 'Networking Income' },
-    { accessorKey: 'networking_expenses', header: 'Networking Expenses' },
-    { accessorKey: 'platform_fees', header: 'Platform Fees' },
-    { accessorKey: 'net_amount', header: 'Total' },
+    {
+      accessorKey: 'settlement_type',
+      header: 'Settlement Type',
+      cell: ({ row }) => {
+        const type = row.original.settlement_type
+        const formatted = type
+          ?.replace(/_/g, ' ') // remove underscore
+          ?.replace(/\b\w/g, l => l.toUpperCase()) // capitalize words
+        return <span>{formatted}</span>
+      }
+    },
+    {
+      accessorKey: 'money_flow',
+      header: 'Settlement Flow',
+      cell: ({ row }) => {
+        const flow = row.original.money_flow
+        return (
+          <Badge
+            label={flow === 'in' ? 'Incoming' : 'Outgoing'}
+            variant={flowVariantMap[flow]}
+          />
+        )
+      }
+    },
+    {
+      accessorKey: 'settlement_date',
+      header: 'Settlement Date',
+      cell: ({ row }) => {
+        return <span>{formatDate(row.original.settlement_date)}</span>
+      }
+    },
+    // { accessorKey: 'membership_income', header: 'Membership Income' },
+    // { accessorKey: 'inventory_income', header: 'Inventory Income' },
+    // { accessorKey: 'networking_income', header: 'Networking Income' },
+    // { accessorKey: 'networking_expenses', header: 'Networking Expenses' },
+    // { accessorKey: 'platform_fees', header: 'Platform Fees' },
+    { accessorKey: 'amount', header: 'Total Amount' },
     {
       accessorKey: 'status',
       header: 'Status',
@@ -31,7 +64,7 @@ const SettlementReport = ({ tableParams, setTableParams }) => {
   return (
     <DataTable
       columns={columns}
-      data={data?.settlements || []}
+      data={data?.data || []}
       tableParams={tableParams}
       setTableParams={setTableParams}
       pagination={data?.total || 0}
