c2e888b4b506acac79eab121aafd4b0531ea5408
chandanac2897 <chandana.c@ibadvisory.com>
2026-04-02 17:37:49 +0530

    added

--- frontend/src/pages/role-permissions/components/ModuleData.jsx
+++ frontend/src/pages/role-permissions/components/ModuleData.jsx
@@ -2,29 +2,29 @@ export const modulesData = [
   {
     id: 'dashboard',
     name: 'Dashboard',
-    submodules: ['Add branch','Create Branch']
+    submodules: ['Add branch', 'Create Branch']
   },
   {
     id: 'employee_management',
     name: 'Employee Management',
     submodules: ['Employee', 'Salary Structure', 'Payroll'],
     sub_submodules: {
-      Employee: ['List', 'Add', 'Edit', 'Delete'],
+      Employee: ['List', 'Add', 'Edit', 'Delete']
     }
   },
   {
     id: 'membership_plan',
     name: 'Membership Plan',
-    submodules: ['List', 'Add', 'Edit', 'Delete','Enable/Disable'],
+    submodules: ['List', 'Add', 'Edit', 'Delete', 'Enable/Disable']
   },
   {
     id: 'crm',
     name: 'CRM',
-    submodules: ['Member','Leads' ,'Guest','Visitor'],
+    submodules: ['Member', 'Leads', 'Guest', 'Visitor'],
     sub_submodules: {
-      Member: ['List', 'Add', 'Edit', 'Delete','Enable/Disable'],
+      Member: ['List', 'Add', 'Edit', 'Delete', 'Enable/Disable'],
       Visitor: ['Add', 'Convert to Member'],
-      Guest: ['Add', 'Convert to Member'],
+      Guest: ['Add', 'Convert to Member']
     }
   },
   {
@@ -39,70 +39,34 @@ export const modulesData = [
   {
     id: 'attendance',
     name: 'Attendance',
-    submodules: ['Member', 'Visitor'],
-    sub_submodules: {
-      Member: ['List', 'Add', 'Edit', 'Delete'],
-      Visitor: ['Add', 'Edit']
-    }
   },
   {
     id: 'wallet',
     name: 'Wallet',
-    submodules: ['Member', 'Visitor'],
-    sub_submodules: {
-      Member: ['List', 'Add', 'Edit', 'Delete'],
-      Visitor: ['Add', 'Edit']
-    }
+    submodules: ['Add TopUP']
   },
   {
     id: 'inventory',
-    name: 'Inventory',
-    submodules: ['Member', 'Visitor'],
-    sub_submodules: {
-      Member: ['List', 'Add', 'Edit', 'Delete'],
-      Visitor: ['Add', 'Edit']
-    }
+    name: 'Inventory'
   },
   {
     id: 'billing',
-    name: 'Billing',
-    submodules: ['Member', 'Visitor'],
-    sub_submodules: {
-      Member: ['List', 'Add', 'Edit', 'Delete'],
-      Visitor: ['Add', 'Edit']
-    }
+    name: 'Billing'
   },
   {
     id: 'account',
-    name: 'Account',
-    submodules: ['Member', 'Visitor'],
-    sub_submodules: {
-      Member: ['List', 'Add', 'Edit', 'Delete'],
-      Visitor: ['Add', 'Edit']
-    }
-  },{
+    name: 'Account'
+  },
+  {
     id: 'branding',
-    name: 'Branding',
-    submodules: ['Member', 'Visitor'],
-    sub_submodules: {
-      Member: ['List', 'Add', 'Edit', 'Delete'],
-      Visitor: ['Add', 'Edit']
-    }
-  },{
+    name: 'Branding'
+  },
+  {
     id: 'report',
-    name: 'Report',
-    submodules: ['Member', 'Visitor'],
-    sub_submodules: {
-      Member: ['List', 'Add', 'Edit', 'Delete'],
-      Visitor: ['Add', 'Edit']
-    }
-  },{
+    name: 'Report'
+  },
+  {
     id: 'role',
-    name: 'Role and Permission',
-    submodules: ['Member', 'Visitor'],
-    sub_submodules: {
-      Member: ['List', 'Add', 'Edit', 'Delete'],
-      Visitor: ['Add', 'Edit']
-    }
+    name: 'Role and Permission'
   }
 ]
