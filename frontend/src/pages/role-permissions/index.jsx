e29de7df96038eb2625e67b600ed2daae836243c
chandanac2897 <chandana.c@ibadvisory.com>
2026-04-07 16:19:43 +0530

    checkbox chnages

--- frontend/src/pages/role-permissions/index.jsx
+++ frontend/src/pages/role-permissions/index.jsx
@@ -317,7 +317,6 @@ const RoleAndPermission = () => {
                       <div className='flex items-center gap-2 p-3'>
                         <input
                           type='checkbox'
-                          className='w-4 h-4'
                           checked={moduleState.enabled || false}
                           onChange={() => toggleModule(module.id)}
                         />
@@ -334,7 +333,6 @@ const RoleAndPermission = () => {
                           <div className='flex items-center gap-2'>
                             <input
                               type='checkbox'
-                              className='w-4 h-4'
                               checked={moduleState.enabled || false}
                               ref={el => {
                                 if (el) {
@@ -364,7 +362,6 @@ const RoleAndPermission = () => {
                                   <label className='flex items-center gap-2'>
                                     <input
                                       type='checkbox'
-                                      className='w-4 h-4'
                                       checked={subState.enabled || false}
                                       onChange={() =>
                                         toggleSubmodule(module.id, sub.id)
