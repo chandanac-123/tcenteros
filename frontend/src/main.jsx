0701338a847b2fff31678151c4b0e9c74eafc565
chandanac2897 <chandana.c@ibadvisory.com>
2026-04-08 14:02:57 +0530

    helmet added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,15 +6,21 @@ import './index.css'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './api/queryClient'
 import { Toaster } from '@pages/components/ui/sonner'
-
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-        <Toaster position='top-center' duration={2000} />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </React.StrictMode>
+  <HelmetProvider>
+    <Helmet>
+      <title>TCenterOS</title>
+      <link rel='canonical' href='https://tcenteros.com/' />
+    </Helmet>
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+          <Toaster position='top-center' duration={2000} />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </React.StrictMode>
+  </HelmetProvider>
 )
