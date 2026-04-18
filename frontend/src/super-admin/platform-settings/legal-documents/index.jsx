fdff66a9294c6ab1d844750f182585e5eadc0bcb
chandanac2897 <chandana.c@ibadvisory.com>
2026-04-18 16:22:59 +0530

    terms

--- frontend/src/super-admin/platform-settings/legal-documents/index.jsx
+++ frontend/src/super-admin/platform-settings/legal-documents/index.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { Button } from "@pages/components/ui/button";
+import { Textarea } from "@pages/components/ui/textarea";
+import { Eye, FileText } from "lucide-react";
+import React from "react";
 
 const LegalDocuments = () => {
   return (
-    <div>ss
-      
-    </div>
-  )
-}
+    <div className="flex flex-col p-4 rounded-lg space-y-4 mt-4 shadow-[0px_5px_15px_rgba(0,0,0,0.35)]">
+      <form>
+        <div className="flex items-center justify-between p-4">
+          <div className="flex  gap-4">
+            <div className=" p-3 rounded-full shadow-[0px_5px_15px_rgba(0,0,0,0.35)]">
+              <FileText size={16} className="text-onboard_primary" />
+            </div>
+            <div className="flex flex-col justify-center gap-2">
+              <p className="text-[#3A3A3A] font-poppins text-[18px] font-semibold leading-[12px]">
+                Terms & Conditions
+              </p>
+              <p className="text-[#393636] font-inter text-[14px] font-medium">
+                Last updated: 2026-04-18
+              </p>
+            </div>
+          </div>
 
-export default LegalDocuments
+          <div className="flex gap-4">
+            <Button size="addbutton" variant="outline_secondary">
+              <Eye />
+              Show Preview
+            </Button>
+          </div>
+        </div>
+
+        <div>
+          <Textarea label="Content" rows={10} />
+        </div>
+        <div className="flex justify-end mt-4">
+          <Button size="addbutton" type="submit">
+            Save Terms & Condition
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LegalDocuments;
