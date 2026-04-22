91dffc89ff6a91a2255fd34cd136ed09ebf6435d
chandanac2897 <chandana.c@ibadvisory.com>
2026-04-22 17:50:05 +0530

    dashboard greeting

--- frontend/src/partner/dashboard/index.jsx
+++ frontend/src/partner/dashboard/index.jsx
@@ -1,12 +1,28 @@
-import ContentLayout from '@common/MasterLayout/ContentLayout'
-import React from 'react'
+import ContentLayout from "@common/MasterLayout/ContentLayout";
+import { getGreeting } from "@utils/helper";
+import React, { useEffect, useState } from "react";
 
 const PartnerDashboard = () => {
+  const [greeting, setGreeting] = useState(getGreeting());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setGreeting(getGreeting());
+    }, 300000); //  every 5 min is enough
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <ContentLayout>
-      aaaaaaaaaaaa
+      <div className="flex flex-col">
+        <span>{greeting}</span>
+        <span className="text-xs">
+          Start managing leads and track your earnings below.
+        </span>
+        
+      </div>
     </ContentLayout>
-  )
-}
+  );
+};
 
-export default PartnerDashboard
+export default PartnerDashboard;
