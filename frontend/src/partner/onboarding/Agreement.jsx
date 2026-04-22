cd40de9d3766fe2beb2029e198ea5a099a6135c2
chandanac2897 <chandana.c@ibadvisory.com>
2026-04-22 15:53:26 +0530

    content

--- frontend/src/partner/onboarding/Agreement.jsx
+++ frontend/src/partner/onboarding/Agreement.jsx
@@ -56,16 +56,42 @@ const Agreement = () => {
     <PartnerLayout>
       <div className="flex h-full w-full flex-col gap-2 overflow-y-auto p-4">
         <HeaderProgress currentStep={2} />
-        <form className=" px-4 gap-4 flex flex-col" onSubmit={formik.handleSubmit}>
+        <form
+          className=" px-4 gap-4 flex flex-col"
+          onSubmit={formik.handleSubmit}
+        >
           <span className="flex text-md font-semibold">
             Resseller Esstential
           </span>
-          <Textarea
+          <span className="flex border rounded-md p-4 text-sm font-medium h-44 overflow-auto">
+            • 15% commission on new sales <br />
+            • 3% commission on renewals <br />
+            • Commission calculated automatically on payment success
+            <br />
+            <br />
+            Lead Policy <br />
+            • Platform provides qualified leads based on your district <br />
+            • You can also generate and manage your own leads <br />
+            • Response SLA: 2 hours for platform-assigned leads <br /> • Lead
+            expiry: 48 hours without status update <br />
+            • Leads may be auto-reassigned if no response <br />
+            <br /> Payout Cycle <br />
+            • Weekly or monthly payout cycles <br />• Commissions tracked in
+            real-time in your dashboard <br />• Minimum payout threshold: ₹1,000{" "}
+            <br /> <br /> Code of Conduct <br />
+            • Represent products accurately and professionally <br />• Maintain
+            ethical sales practices <br /> • Respond to assigned leads promptly
+             <br/>• No misrepresentation of pricing or features Refund & Cancellation{" "}
+            <br /> • Onboarding fee of ₹2,500 is non-refundable <br />• Reseller
+            account can be deactivated after 60 days of inactivity <br />• You
+            can reactivate your account anytime
+          </span>
+          {/* <Textarea
             label=""
             rows={8}
             value="By continuing, you confirm that you understand and accept the reseller terms, commission rules, lead handling policy, and code of conduct."
             readOnly
-          />
+          /> */}
 
           <div className="flex items-center gap-4">
             <Checkbox
