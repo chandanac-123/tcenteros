adb27720f19abedcc7dd3f3821414cc1c10ddc3c
chandanac2897 <chandana.c@ibadvisory.com>
2026-04-29 14:25:08 +0530

    hybrid mode hide

--- frontend/src/constants/classMode.js
+++ frontend/src/constants/classMode.js
@@ -3,5 +3,5 @@ import hybrid from '@assets/images/hybrid.svg'
 
 export const classModes = [
   { id: 'in-person', name: 'In-Person', image_url: inperson },
-  { id: 'hybrid', name: 'Hybrid', image_url: hybrid },
+  // { id: 'hybrid', name: 'Hybrid', image_url: hybrid },
 ]
