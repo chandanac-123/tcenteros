190f05700cccce68e6c38292be0276dd7c1f233b
chandanac2897 <chandana.c@ibadvisory.com>
2026-04-29 17:46:57 +0530

    chat

--- frontend/src/pages/notifications/components/Tickets.jsx
+++ frontend/src/pages/notifications/components/Tickets.jsx
@@ -1,64 +1,64 @@
-import { Button } from '@pages/components/ui/button'
-import Chat from './Chat'
-import { useState } from 'react'
-import { useAllTicketsQuery } from '@api-queries/center-admin/notifictaions/Query'
-import CloseTicket from './TicketClose'
-import { Spinner } from '@pages/components/ui/spinner'
+import { Button } from "@pages/components/ui/button";
+import Chat from "./Chat";
+import { useState } from "react";
+import { useAllTicketsQuery } from "@api-queries/center-admin/notifictaions/Query";
+import CloseTicket from "./TicketClose";
+import { Spinner } from "@pages/components/ui/spinner";
 
 const Tickets = () => {
-  const [openChat, setOpenChat] = useState(false)
-  const [selectedTicket, setSelectedTicket] = useState(null)
-  const [ticketClose, setTicketClose] = useState(false)
-  const { data: tickets, isLoading } = useAllTicketsQuery()
+  const [openChat, setOpenChat] = useState(false);
+  const [selectedTicket, setSelectedTicket] = useState(null);
+  const [ticketClose, setTicketClose] = useState(false);
+  const { data: tickets, isLoading } = useAllTicketsQuery();
 
   return (
-    <>
-      {isLoading && (
+    <div className="flex justify-between gap-4 h-[calc(100vh-220px)] ">
+      {/* {isLoading && (
         <div className='flex justify-center'>
           <Spinner />
         </div>
-      )}
-      {!openChat &&
-        tickets?.map(ticket => (
+      )} */}
+      <div className="flex-1 ">
+        {tickets?.map((ticket) => (
           <div
             key={ticket.id}
-            className='flex border border-tab_bg rounded-lg p-3 mb-3'
+            className="flex-1 border  border-tab_bg rounded-lg p-3 mb-3"
           >
-            <div className='flex flex-col w-full gap-2'>
-              <div className='flex justify-between w-full'>
-                <span className='text-md text-textblack font-semibold'>
+            <div className="flex flex-col w-full gap-2">
+              <div className="flex justify-between w-full">
+                <span className="text-md text-textblack font-semibold">
                   {ticket.subject}
                 </span>
 
-                <span className='text-xs text-textgrey'>
+                <span className="text-xs text-textgrey">
                   {new Date(ticket.created_at).toLocaleString()}
                 </span>
               </div>
 
-              <div className='flex justify-between w-full items-center'>
-                <span className='text-xs text-textgrey'>
+              <div className="flex justify-between w-full items-center">
+                <span className="text-xs text-textgrey">
                   {ticket.description}
                 </span>
 
-                <div className='flex gap-2'>
+                <div className="flex gap-2">
                   <Button
-                    variant='outline_primary'
-                    size='notificationbutton'
-                    className='text-xs'
-                    disabled={ticket?.status === 'closed'}
+                    variant="outline_primary"
+                    size="notificationbutton"
+                    className="text-xs"
+                    disabled={ticket?.status === "closed"}
                     onClick={() => {
-                      setSelectedTicket(ticket.id)
-                      setTicketClose(true)
+                      setSelectedTicket(ticket.id);
+                      setTicketClose(true);
                     }}
                   >
                     Close
                   </Button>
                   <Button
-                    size='notificationbutton'
-                    className='text-xs'
+                    size="notificationbutton"
+                    className="text-xs"
                     onClick={() => {
-                      setSelectedTicket(ticket.id)
-                      setOpenChat(true)
+                      setSelectedTicket(ticket.id);
+                      setOpenChat(true);
                     }}
                   >
                     Open
@@ -69,18 +69,24 @@ const Tickets = () => {
           </div>
         ))}
 
-      {openChat && (
+        {/* {openChat && (
         <div className='mt-4 h-[500px]'>
           <Chat ticketId={selectedTicket} />
         </div>
-      )}
-      <CloseTicket
-        open={ticketClose}
-        setOpen={setTicketClose}
-        id={selectedTicket}
-      />
-    </>
-  )
-}
+      )} */}
 
-export default Tickets
+        <CloseTicket
+          open={ticketClose}
+          setOpen={setTicketClose}
+          id={selectedTicket}
+        />
+      </div>
+
+      <div className="flex-1">
+        <Chat ticketId={selectedTicket} />
+      </div>
+    </div>
+  );
+};
+
+export default Tickets;
