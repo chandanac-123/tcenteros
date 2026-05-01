205cf80e749fdd056be46ff03a811c466ecd4f99
chandanac2897 <chandana.c@ibadvisory.com>
2026-05-01 17:47:47 +0530

    icon

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -35,8 +35,10 @@ import AddBranches from "@pages/branch/AddBranches"
 import ProfilePage from '@pages/profile'
 
 import {
+  BanknoteArrowUp,
   Boxes,
   Building2,
+  CalendarSync,
   ChartPie,
   CircleDollarSign,
   CrownIcon,
@@ -186,6 +188,6 @@ export const routes = [
   { key: 100, path: '/lead-management', privetRoute: true, isSubRoute: false, pageTitle: 'Leads Management', component: LeadsManagement, icon: <UserRoundPen />, menubar: true, permissionKey: true, isPartner: true },
   { key: 101, path: '/lead-management/add-newLeads', privetRoute: true, isSubRoute: false, pageTitle: 'Partners-Leads', component: AddnewLeads, icon: <UserRoundPen />, menubar: false, permissionKey: true, isPartner: true },
 
-  { key: 60, path: '/earning-payout', privetRoute: true, isSubRoute: false, pageTitle: 'Earnings & Payouts', component: EarningsAndPayouts, icon: <UserRoundCog />, menubar: true, permissionKey: true, isPartner: true },
-  { key: 61, path: '/renewal', privetRoute: true, isSubRoute: false, pageTitle: 'Renewals', component: Renewals, icon: <SettingsIcon />, menubar: true, permissionKey: true, isPartner: true },
+  { key: 60, path: '/earning-payout', privetRoute: true, isSubRoute: false, pageTitle: 'Earnings & Payouts', component: EarningsAndPayouts, icon: <BanknoteArrowUp />, menubar: true, permissionKey: true, isPartner: true },
+  { key: 61, path: '/renewal', privetRoute: true, isSubRoute: false, pageTitle: 'Renewals', component: Renewals, icon: <CalendarSync />, menubar: true, permissionKey: true, isPartner: true },
 ]
