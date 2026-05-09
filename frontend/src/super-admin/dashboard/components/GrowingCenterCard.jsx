6c04f700baa039d103ffd3f71d08c17ed13d1b50
chandanac2897 <chandana.c@ibadvisory.com>
2026-05-09 17:32:12 +0530

    img dashboard

--- frontend/src/super-admin/dashboard/components/GrowingCenterCard.jsx
+++ frontend/src/super-admin/dashboard/components/GrowingCenterCard.jsx
@@ -5,9 +5,11 @@ const GrowingCenterCard = ({ data }) => {
     <div className="w-full h-20 border cursor-pointer flex flex-col justify-center p-3 bg-textwhite rounded-xl shadow-primary-shadow">
       <div className="flex items-center justify-between w-full">
         <div className="flex items-center gap-3">
-          <div className=" p-2 text-onboard_primary rounded-full border shadow-[0px_5px_15px_rgba(0,0,0,0.10)]">
-            {data?.icon}
-          </div>
+          <img
+            src={data?.center_image}
+            alt=""
+            className="w-16 h-16 rounded-full  border shadow-[0px_5px_15px_rgba(0,0,0,0.10)]"
+          />
           <div>
             <span className="text-sm font-medium text-grey">
               {data?.center_name}
