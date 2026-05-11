701eccb3f5a200be0c31a34ca7061516f8ba885a
chandanac2897 <chandana.c@ibadvisory.com>
2026-05-11 15:20:41 +0530

    dashboard table remove

--- frontend/src/pages/Dashboard/CenterAdminDashboard.jsx
+++ frontend/src/pages/Dashboard/CenterAdminDashboard.jsx
@@ -246,7 +246,6 @@ const CenterAdminDashboard = () => {
             />
           </Card>
         </div>
-        {/* <DashboardTable /> */}
       </div>
       <InstructionPage open={instructionOpen} setOpen={setInstructionOpen} />
       <RenewSubcription
