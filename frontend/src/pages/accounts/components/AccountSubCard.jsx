1131ca2730bd74bd5784f770d7baa3497270d977
chandanac2897-oss <chandana@ibadvisory.com>
2026-05-25 09:50:40 +0530

    card clickable

--- frontend/src/pages/accounts/components/AccountSubCard.jsx
+++ frontend/src/pages/accounts/components/AccountSubCard.jsx
@@ -1,32 +1,17 @@
-import expense from '@assets/account-modules/expense.svg'
-import income from '@assets/account-modules/income.svg'
-import inventory from '@assets/account-modules/inventory.svg'
-import ledger from '@assets/account-modules/ledger.svg'
-import overview from '@assets/account-modules/overview.svg'
-import payroll from '@assets/account-modules/payroll.svg'
-import settlement from '@assets/account-modules/settlement.svg'
-import taxes from '@assets/account-modules/taxes.svg'
+import { subaccounts } from '@constants/accounts_submodules'
+import { useNavigate } from 'react-router-dom'
 
 const AccountSubCard = () => {
-  const subaccounts = [
-    { title: 'Overview', image: overview },
-    { title: 'Ledger', image: ledger },
-    { title: 'Income', image: income },
-    { title: 'Expenses', image: expense },
-    { title: 'Payroll', image: payroll },
-    { title: 'Inventory', image: inventory },
-    { title: 'Settlement', image: settlement },
-    { title: 'Taxes', image: taxes }
-  ]
-
+  const navigate = useNavigate()
   return (
     <div className='flex flex-wrap gap-4 w-full justify-center items-center'>
       {subaccounts.map(item => (
         <div
+          onClick={() => navigate(`/accounts/sub-modules/${item.title.toLowerCase()}`)}
           key={item.title}
           className='flex flex-col w-24 items-center gap-4 border border-tab_bg rounded-lg p-3 hover:shadow-sm transition'
         >
-          <img src={item.image} alt={item.title} loading="lazy"/>
+          <img src={item.image} alt={item.title} loading="lazy" />
           <span className='text-sm font-medium text-muted-foreground'>
             {item.title}
           </span>
--- frontend/src/pages/accounts/sub-modules/index.jsx
+++ frontend/src/pages/accounts/sub-modules/index.jsx
@@ -1,57 +1,69 @@
 import ContentLayout from '@common/MasterLayout/ContentLayout'
-import { useState } from 'react'
 import { subaccounts } from '@constants/accounts_submodules'
 import { ChevronsLeft } from 'lucide-react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const AccountsSubModules = () => {
-  const [activeIndex, setActiveIndex] = useState(0)
   const navigate = useNavigate()
+  const { module } = useParams()
+
+  // Find active tab from URL
+  const activeIndex = subaccounts.findIndex(
+    item => item.title.toLowerCase() === module
+  )
+
+  // fallback to first tab
+  const currentIndex = activeIndex >= 0 ? activeIndex : 0
 
   return (
     <ContentLayout>
       <div className='flex flex-col gap-2'>
         <span className='text-lg font-semibold text-textblack'>
-          Accounts Sub Modules - {subaccounts[activeIndex].title}
+          Accounts Sub Modules - {subaccounts[currentIndex].title}
         </span>
-        {/* Tabs Container */}
+
         <button
           className='flex gap-1 border-none bg-transparent text-primary items-center mb-2'
           onClick={() => navigate('/accounts')}
         >
           <ChevronsLeft /> Back to Accounts
         </button>
-        <div className='bg-white rounded-xl border border-gray-200 flex overflow-x-auto '>
+
+        {/* Tabs */}
+        <div className='bg-white rounded-xl border border-gray-200 flex overflow-x-auto'>
           {subaccounts.map((item, index) => {
-            const isActive = activeIndex === index
+            const isActive = currentIndex === index
+
             return (
               <button
                 key={index}
-                onClick={() => setActiveIndex(index)}
+                onClick={() =>
+                  navigate(
+                    `/accounts/sub-modules/${item.title.toLowerCase()}`
+                  )
+                }
                 className={`
                   flex flex-col items-center justify-center
                   w-full
                   py-4 px-6
                   border-r border-gray-200 last:border-r-0
                   transition-all duration-200
-                  ${
-                    isActive
-                      ? `${item.active_bg_color} text-white`
-                      : 'bg-white hover:bg-gray-50'
+                  ${isActive
+                    ? `${item.active_bg_color} text-white`
+                    : 'bg-white hover:bg-gray-50'
                   }
                 `}
               >
                 <img
                   src={isActive ? item.image_active : item.image}
                   alt={item.title}
-                  loading="lazy"
+                  loading='lazy'
                   className='w-8 h-8 mb-2'
                 />
 
                 <span
-                  className={`text-sm font-medium ${
-                    isActive ? 'text-white' : 'text-gray-700'
-                  }`}
+                  className={`text-sm font-medium ${isActive ? 'text-white' : 'text-gray-700'
+                    }`}
                 >
                   {item.title}
                 </span>
@@ -59,8 +71,11 @@ const AccountsSubModules = () => {
             )
           })}
         </div>
-        {/* Active Module Content */}
-        <div className='mt-4'>{subaccounts[activeIndex].component}</div>
+
+        {/* Active Component */}
+        <div className='mt-4'>
+          {subaccounts[currentIndex].component}
+        </div>
       </div>
     </ContentLayout>
   )
