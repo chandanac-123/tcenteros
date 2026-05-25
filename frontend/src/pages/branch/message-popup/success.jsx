4727fe5ef9f38f883e643d479f11cbb1c387cac5
chandanac2897-oss <chandana@ibadvisory.com>
2026-05-25 14:20:35 +0530

    peyment success

--- frontend/src/pages/branch/message-popup/success.jsx
+++ frontend/src/pages/branch/message-popup/success.jsx
@@ -11,13 +11,6 @@ const SuccessModal = ({ open, onOpenChange, count }) => {
             <div className="flex flex-col items-center justify-center gap-2">
                 <img src={success} alt="view" loading="lazy"/>
                 <h3 className='font-semibold'>Payment Successful</h3>
-                <button
-                    type="button"
-                    onClick={() => navigate('/dashboard')}
-                    className='border-[1px] text-[#6D758F] border-[#6D758F] w-full p-2 rounded-md '
-                >
-                    Back to Dashboard
-                </button>
             </div>
         </CustomeModal>
     )
